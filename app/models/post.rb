447579f5e602c319f8e4ce86745bf1eb460f0cad
Chad Lengefeld <chadlengefeld@gmail.com>
2021-01-02 07:07:46 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,31 +1,12 @@
 class Post < ActiveRecord::Base
-
-  validate :is_title_case 
-  before_validation :make_title_case 
   belongs_to :author
 
-  #put new code here
-  def self.from_today
-    where("created_at >=?", Time.zone.today.beginning_of_day)
+  def author_name
+    self.try(:author).try(:name)
   end
 
-  def self.old_news
-    where("created_at <?", Time.zone.today.beginning_of_day)
-  end
-
-  def self.by_author(author_id)
-    where(author: author_id)
-  end
-
-  private
-
-  def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
-      errors.add(:title, "Title must be in title case")
-    end
-  end
-
-  def make_title_case
-    self.title = self.title.titlecase
+  def author_name=(name)
+    author = Author.find_or_create_by(name: name)
+    self.artist = artist
   end
 end
